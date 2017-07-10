23e8dc82b9c6c3001f30b34fcbc48a4b1d8e9e41
ATRIGNA <ATRIGNA@C415MCND6431QXW.detss.corpintra.net>
2017-07-10 12:20:03 +0200

    Information updated
    - updated version info
    - updated copyright notice
    - added mising information about licensing etc.
    - plugin and feature now in sync again

--- basheditor-plugin/src/main/java-eclipse/de/jcup/basheditor/preferences/BashEditorSyntaxColorPreferencePage.java
+++ basheditor-plugin/src/main/java-eclipse/de/jcup/basheditor/preferences/BashEditorSyntaxColorPreferencePage.java
@@ -1,6 +1,6 @@
 package de.jcup.basheditor.preferences;
 /*
- * Copyright 2016 Albert Tregnaghi
+ * Copyright 2017 Albert Tregnaghi
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
