3467b607a55048ea92e49b58e07acccefb228235
Albert Tregnaghi <albert.tregnaghi@gmail.com>
2017-07-11 09:17:27 +0200

    added built in commands #2

--- basheditor-plugin/src/main/java-eclipse/de/jcup/basheditor/document/keywords/BashLanguageKeyWords.java
+++ basheditor-plugin/src/main/java-eclipse/de/jcup/basheditor/document/keywords/BashLanguageKeyWords.java
@@ -1,13 +1,13 @@
 /*
  * Copyright 2017 Albert Tregnaghi
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the"License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *		http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an"AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions
  * and limitations under the License.
@@ -58,12 +58,66 @@ public enum BashLanguageKeyWords implements DocumentKeyWord {
 
 	
 
-	/* Other commands */
-	LOCAL("local"),	
-	SHIFT("shift"),	
-	READ("read"),	
-	RETURN("return"),
-	GOTO("goto"), // #?'!?
+	/* Built in commands */
+	// see https://askubuntu.com/questions/512918/how-do-i-list-all-available-shell-builtin-commands
+	ALIAS                 ("alias"),
+	BG                    ("bg"),
+	BIND                  ("bind"),
+	BREAK                 ("break"),
+	BUILTIN               ("builtin"),
+	CALLER                ("caller"),
+	CD                    ("cd"),
+	COMMAND               ("command"),
+	COMPGEN               ("compgen"),
+	COMPLETE              ("complete"),
+	COMPOPT               ("compopt"),
+	CONTINUE              ("continue"),
+	DECLARE               ("declare"),
+	DIRS                  ("dirs"),
+	DISOWN                ("disown"),
+	ECHO                  ("echo"),
+	ENABLE                ("enable"),
+	EVAL                  ("eval"),
+	EXEC                  ("exec"),
+	EXIT                  ("exit"),
+	EXPORT                ("export"),
+	FALSE                 ("false"),
+	FC                    ("fc"),
+	FG                    ("fg"),
+	GETOPTS               ("getopts"),
+	HASH                  ("hash"),
+	HELP                  ("help"),
+	HISTORY               ("history"),
+	JOBS                  ("jobs"),
+	KILL                  ("kill"),
+	LET                   ("let"),
+	LOCAL                 ("local"),
+	LOGOUT                ("logout"),
+	MAPFILE               ("mapfile"),
+	POPD                  ("popd"),
+	PRINTF                ("printf"),
+	PUSHD                 ("pushd"),
+	PWD                   ("pwd"),
+	READ                  ("read"),
+	READARRAY             ("readarray"),
+	READONLY              ("readonly"),
+	RETURN                ("return"),
+	SET                   ("set"),
+	SHIFT                 ("shift"),
+	SHOPT                 ("shopt"),
+	SOURCE                ("source"),
+	SUSPEND               ("suspend"),
+	TEST                  ("test"),
+	TIMES                 ("times"),
+	TRAP                  ("trap"),
+	TRUE                  ("true"),
+	TYPE                  ("type"),
+	TYPESET               ("typeset"),
+	ULIMIT                ("ulimit"),
+	UMASK                 ("umask"),
+	UNALIAS               ("unalias"),
+	UNSET                 ("unset"),
+	WAIT                  ("wait"),
 	;
 	/* @formatter:on*/
 
