fc3abbdb4ee21045a56be5126d58f1d5064b97a4
de-jcup <albert.tregnaghi@gmail.com>
2020-06-06 01:35:13 +0200

    Change deployment to automatically sign plugin #200

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 BINTRAY_USER=$1
 BINTRAY_API_KEY=$2
+BINTRAY_VERSION=$3
 
 function show_help_and_exit() {
 	echo "Usage:"
-	echo " deploy.sh bintrayUser apiKey"
+	echo " deploy.sh bintrayUser apiKey version"
 	exit 1
 }
 
@@ -16,4 +17,18 @@ if [ -z "$BINTRAY_API_KEY" ]; then
 	echo "bintray api key not set"
 	show_help_and_exit;
 fi
-./pushToBintray.sh ${BINTRAY_USER} ${BINTRAY_API_KEY} de-jcup basheditor update-site 2.2.3 ./basheditor-updatesite/
+
+if [ -z "$BINTRAY_VERSION" ]; then
+    echo "bintray version not set"
+    show_help_and_exit;
+fi
+
+# call jar signing before pushing to bintray...
+export KEYSTORE_PWD
+export KEYSTORE_LOCATION
+
+set -e
+
+./signJars
+
+./pushToBintray.sh ${BINTRAY_USER} ${BINTRAY_API_KEY} de-jcup basheditor update-site ${BINTRAY_VERSION}  ./basheditor-updatesite/
